368b7e6480a45e6fee3f76bc3069175d47306b29
Alexander <alexander.nguyen@invokelabs.com>
2013-12-04 14:57:17 -0800

    added lolnexus logo

--- content/overlay-cards/main-newsfeed/lolscouter.php
+++ content/overlay-cards/main-newsfeed/lolscouter.php
@@ -3,7 +3,7 @@
 <div class="client-pin">
   <h4 class="title"><b>LoL Scouter</b></h4>
   <p class="info">
-    <img src="assets/images/website_sources/twitchtv.png" height="18px" width="18px">
+    <img src="assets/images/website_sources/lolnexus.png" height="18px" width="18px">
     LoL Nexus
   </p>
   <div class="client-pin-content-holder">
@@ -12,17 +12,17 @@
       <input name="search" type="text" class="search-query" placeholder="Enter a player currently in match">
       <!-- <div class="btn-toolbar">
         <div class="btn-group">
-          <button class="btn">NA</button>
-          <button class="btn">EUW</button>
-          <button class="btn">EUNE</button>
-          <button class="btn">BR</button>
-          <button class="btn">TR</button>
+          <button name="NA" class="btn">NA</button>
+          <button name="EUW" class="btn">EUW</button>
+          <button name="EUNE" class="btn">EUNE</button>
+          <button name="BR" class="btn">BR</button>
+          <button name="TR" class="btn">TR</button>
         </div>
         <div class="btn-group">
-          <button class="btn">RU</button>
-          <button class="btn">LAN</button>
-          <button class="btn">LAS</button>
-          <button class="btn">OCE</button>
+          <button name="RU" class="btn">RU</button>
+          <button name="LAN" class="btn">LAN</button>
+          <button name="LAS" class="btn">LAS</button>
+          <button name="OCE" class="btn">OCE</button>
         </div>
       </div> -->
     </form>
