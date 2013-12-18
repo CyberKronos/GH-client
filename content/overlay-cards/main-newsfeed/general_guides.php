d60a2d40770d39c72ae35d1fc48318ea59fdb966
Alexander <alexander.nguyen@invokelabs.com>
2013-12-17 16:57:18 -0800

    - WIP général guides card

---
+++ content/overlay-cards/main-newsfeed/general_guides.php
@@ -0,0 +1,42 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/GH-client/assets/inc/incfiles/client-header.inc.php'); ?>
+
+<div class="client-pin">
+  <h4 class="title"><b>General Guides</b></h4>
+  <p class="info">
+    <img src="assets/images/website_sources/solomid.png" height="18px" width="18px">
+    Solomid
+  </p>
+  <div class="client-pin-content-holder">
+
+    <!-- TABBED MENU -->
+    <div class="guide-categories"> <!-- Only required for left/right tabs -->
+      <ul class="nav nav-tabs">
+        <li class="active"><a href="#tab1" data-toggle="tab">Runes</a></li>
+        <li><a href="#tab2" data-toggle="tab">Items</a></li>
+        <li><a href="#tab3" data-toggle="tab">Skills</a></li>
+        <li><a href="#tab2" data-toggle="tab">Mastery</a></li>
+      </ul>
+      <div class="tab-content">
+        <div class="tab-pane active" id="tab1">
+          <p>I'm in Section 1.</p>
+        </div>
+        <div class="tab-pane" id="tab2">
+          <p>Howdy, I'm in Section 2.</p>
+        </div>
+        <div class="tab-pane" id="tab3">
+          <p>Howdy, I'm in Section 3.</p>
+        </div>
+        <div class="tab-pane" id="tab4">
+          <p>Howdy, I'm in Section 4.</p>
+        </div>
+        <div class="tab-pane" id="tab5">
+          <p>Howdy, I'm in Section 5.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+</body>
+</html>
