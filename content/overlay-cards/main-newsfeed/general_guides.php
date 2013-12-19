e4c55d874dab658e0bfd852b2ffe5cb13e49337e
Alexander <alexander.nguyen@invokelabs.com>
2013-12-18 17:03:13 -0800

    added more to general guides card

--- content/overlay-cards/main-newsfeed/general_guides.php
+++ content/overlay-cards/main-newsfeed/general_guides.php
@@ -13,24 +13,60 @@
       <ul class="nav nav-tabs">
         <li class="active"><a href="#tab1" data-toggle="tab">Runes</a></li>
         <li><a href="#tab2" data-toggle="tab">Items</a></li>
-        <li><a href="#tab3" data-toggle="tab">Skills</a></li>
-        <li><a href="#tab2" data-toggle="tab">Mastery</a></li>
+        <li><a href="#tab3" data-toggle="tab">Mastery</a></li>
       </ul>
       <div class="tab-content">
+
         <div class="tab-pane active" id="tab1">
-          <p>I'm in Section 1.</p>
+          <ul class="nav nav-pills">
+            <li class="active"><a href="#sub-tab-runes1" data-toggle="tab">AD</a></li>
+            <li><a href="#sub-tab-runes2" data-toggle="tab">AP</a></li>
+            <li><a href="#sub-tab-runes3" data-toggle="tab">Bruiser/Jungle</a></li>
+            <li><a href="#sub-tab-runes4" data-toggle="tab">Support</a></li>
+          </ul>
+          <div class="tab-content">
+            <div class="tab-pane active" id="sub-tab-runes1">
+              <img src="assets/images/guides/general_guides/runes/flat_ad.jpg">
+            </div>
+            <div class="tab-pane" id="sub-tab-runes2">
+              <p>Howdy, I'm in Section 2.</p>
+            </div>
+            <div class="tab-pane" id="sub-tab-runes3">
+              <p>Howdy, I'm in Section 3.</p>
+            </div>
+            <div class="tab-pane" id="sub-tab-runes4">
+              <p>Howdy, I'm in Section 4.</p>
+            </div>
+          </div>
         </div>
+
         <div class="tab-pane" id="tab2">
           <p>Howdy, I'm in Section 2.</p>
         </div>
+
         <div class="tab-pane" id="tab3">
-          <p>Howdy, I'm in Section 3.</p>
+          <ul class="nav nav-pills">
+            <li class="active"><a href="#sub-tab-masteries1" data-toggle="tab">AD</a></li>
+            <li><a href="#sub-tab-masteries2" data-toggle="tab">AP</a></li>
+            <li><a href="#sub-tab-masteries3" data-toggle="tab">Bruiser/Jungle</a></li>
+            <li><a href="#sub-tab-masteries4" data-toggle="tab">Support</a></li>
+          </ul>
+          <div class="tab-content">
+            <div class="tab-pane active" id="sub-tab-masteries1">
+                <p>Howdy, I'm in Section 1.</p>
+            </div>
+            <div class="tab-pane" id="sub-tab-masteries2">
+              <p>Howdy, I'm in Section 2.</p>
+            </div>
+            <div class="tab-pane" id="sub-tab-masteries3">
+              <p>Howdy, I'm in Section 3.</p>
+            </div>
+            <div class="tab-pane" id="sub-tab-masteries4">
+              <p>Howdy, I'm in Section 4.</p>
+            </div>
+          </div>
+
         </div>
-        <div class="tab-pane" id="tab4">
-          <p>Howdy, I'm in Section 4.</p>
-        </div>
-        <div class="tab-pane" id="tab5">
-          <p>Howdy, I'm in Section 5.</p>
         </div>
       </div>
     </div>
