651434dd92bd66c520fd5804105bd600ec38a24e
Vincent Tang <vincent55tang@gmail.com>
2014-01-26 21:04:47 -0800

    n

---
+++ cards/LoL/lol_general_news/announcing_the_sunday_morning_lolz_contest.php
@@ -0,0 +1,62 @@
+
+<?php include(C:/xampp/htdocs/GH-client/assets/inc/incfiles/client-header.inc.php'); ?>
+<div class='client-pin'>
+  <h4 class='title'><b>Announcing the Sunday Morning LoLz Contest</b></h4>
+  <h5 class='sub-title'><b>Contests</b></h5>
+  
+  <div class='client-pin-content-holder'>
+  	<div>
+      <img src=http://na.leagueoflegends.com/sites/default/files/styles/scale_xlarge/public/upload/riotpenguincomicwebsmall.jpg?itok=jF8azJG1>
+    </div>
+    <div>   </div>
+  </div>
+  <br/>
+  <div class='accordion' id='accordion2'>
+    <div class='accordion-group'>
+      <div class='accordion-heading'>
+        <a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion2' href='#collapseOne'>
+          View More
+        </a>
+      </div>
+      <div id='collapseOne' class='accordion-body collapse'>
+        <div class='accordion-inner'>
+          <div class="field field-name-body field-type-text-with-summary field-label-hidden"><p>Some of our best weeks of 2013 began cuddling up with a plush Tibbers, a cup of coffee and the newest Sunday Morning LoLz. Stuffed pillows and caffeine-dispensers have been acquired in 2014, so this contest is the flashing light in the sky begging for more laughs from you!</p>
+<hr class="divider"><h4>The rules:</h4>
+
+<ul><div class="inline-right default-5-12"><span class="content-border"> <a class="lightbox cboxElement" href="http://riot-web-static.s3.amazonaws.com/images/news/January_2014/SMLC/RPC.jpg"> <img src="http://riot-web-static.s3.amazonaws.com/images/news/January_2014/SMLC/RPC.jpg"></a></span></div>
+
+<li><strong>Be LoL-inspired:</strong> in addition to making us giggle loud enough to disturb neighbors, your comic must have a clear League of Legends theme.</li>
+<li><strong>Use the template:</strong> while many of us doodled our way to artistic mastery coloring outside the lines, this time we’re going to be sticklers and insist that your comic is created using our template file and submitted as a <strong>JPG</strong> (or <strong>JPEG</strong>). Choose from the horizontal (<a href="http://riot-web-static.s3.amazonaws.com/images/news/January_2014/SMLC/LoL_Comic_HorizontalTemplate_Wht.jpg" target="_blank"><strong>JPG</strong></a>, <a href="http://riot-web-static.s3.amazonaws.com/images/news/January_2014/SMLC/LoL_Comic_HorizontalTemplate_Wht.psd" target="_blank"><strong>PSD</strong></a>) or vertical (<a href="http://riot-web-static.s3.amazonaws.com/images/news/January_2014/SMLC/LoL_Comic_VerticalTemplate_Wht.jpg" target="_blank"><strong>JPG</strong></a>, <a href="http://riot-web-static.s3.amazonaws.com/images/news/January_2014/SMLC/LoL_Comic_VerticalTemplate_Wht.psd" target="_blank"><strong>PSD</strong></a>) format, but if your creative craving calls for another dimension or a novel-sized graphic opus, this isn’t the contest for it. Pixel dimensions must be identical to whatever template you choose.</li>
+<li><strong>Trinket time:</strong> In the 2014 season, everybody’s got new vision toys to tinker with. It doesn’t have to be the center of attention, but you gotta draw, paint or mention at least one of the new trinkets in your tale.</li>
+<li><strong>Don’t cheat:</strong> these contests are about creativity, so tracing outdated online comics won’t win you points; submit your original work, created in the contest timeframe, or face the wrath of our elite team of anti-fraud ninjas.</li>
+<li><strong>Keep it clean:</strong> we all enjoy a dirty joke now and then, but for equal-opportunity fanfare and celebration, these comics need to be age-appropriate for the youngest players.</li>
+<li><strong>Size matters:</strong> We’ve got all sorts of new-fangled gadgets that compress and unzip things, but for this contest, let’s agree that <strong>10 MB</strong> is the max.</li>
+<li><strong>Be colorful:</strong> like dinosaurs and power rangers, we’re drawn to bright colors, so you can imagine how to catch our eye (hint: bright colors are the way to catch our eye).</li>
+<li><strong>If you MUST use words, English language is preferred:</strong> There are tons of ways to get a laugh that don’t need words at all; we want to emphasize that while the disqualification pass and community reviewers for the contest will primarily be English-language readers, the contest is open to all eligible players!</li>
+<li><strong>Sign your work with your Summoner name:</strong> We’re including a nifty blank space for your summoner name, and we’d really appreciate it if you’d use it. By which we mean you’ve got to sign your art if you want to win.</li>
+<li><strong>No Photos or Screenshots</strong>: We love cosplay, and awesome stories in photo form can be some of the best around, but for this contest, any photographic or screenshot based “comics” would be disqualified.</li>
+</ul><p>Check out Riot Penguin’s comic up there at the top for an example that follows all the rules.</p>
+<p>If you’ve followed the rules, and you want to know how to actually win the contest, here’s how it shakes out:</p>
+<p>Once all of the submissions are in, we’ll run through the gamut to check if they’ve all followed the rules from above. Next, we’ll hold a community vote on all of the remaining qualified comics; so long as the entry isn’t disqualified for breaking the rules, it’ll be a candidate. Finally, we’ll announce the winners, which will include two entries selected by Riot in addition to the victorious comics voted on by the community.</p>
+<hr class="divider"><h4>Now let’s take a look at what’s at stake:</h4>
+<p><strong>Five (Two from Riot and Three From the Community) Grand Prize winners will snag:</strong></p>
+<ul><li><a href="https://store.wacom.com/us/en/product/PTH651?" target="_blank"><strong>Wacom Intuos Pro Pen &amp; Touch Medium</strong></a></li>
+<li>30,000 RP</li>
+</ul><p><strong>Five Runner Up winners will snag:</strong></p>
+<ul><li><a href="https://store.wacom.com/us/en/product/CTH480S2?" target="_blank"><strong>Wacom Intuos Manga Pen &amp; Touch Small</strong></a></li>
+<li>25,000 RP</li>
+</ul><p><strong>Five Honorable Mention winners will snag:</strong></p>
+<ul><li>20,000 RP</li>
+</ul><p>The contest starts on <strong>January 25th</strong> and runs through <strong>February 15th at 11:59 PM Pacific Time</strong>. <a href="http://woobox.com/o5pz8u" target="_blank"><strong>Submit your entries here</strong></a>. Judging for the contest will begin on <strong>February 17th</strong> and end on <strong>February 24 at 11:59 PM Pacific Time</strong>.<br><br> Like any contest, this comes with the usual <a href="http://na.leagueoflegends.com/en/page/sunday-morning-lolz-contest-official-rules" target="_blank"><strong>official rules and legal small print</strong></a>, so be sure to do your due diligence and check them out before you get started!</p>
+<p>Break out the pencils, paintbrushes, crayons, markers, pastels or their hex-tech equivalents; it’s time to share your comic LoLz.</p>
+<p>Got questions? We’ve got an <a href="https://support.leagueoflegends.com/entries/26133575-Sunday-Morning-LoLz-FAQ" target="_blank"><strong>FAQ here</strong></a> and we’ll be answering questions in the comments below.</p></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</div>
+</body>
+</html>
---
+++ cards/LoL/lol_general_news/new_free_champion_rotation_caitlyn_taric_ahri_and_more.php
@@ -0,0 +1,34 @@
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/GH-client/assets/inc/incfiles/client-header.inc.php'); ?>
+<div class='client-pin'>
+  <h4 class='title'><b>New free champion rotation: Caitlyn, Taric, Ahri and more!</b></h4>
+  <h5 class='sub-title'><b>Free Rotation</b></h5>
+  
+  <div class='client-pin-content-holder'>
+  	<div>
+      <img src=http://na.leagueoflegends.com/sites/default/files/styles/scale_xlarge/public/upload/lol.com_.banner.article.f2protation.jan13awejk4.jpg?itok=lmPBYeKz>
+    </div>
+    <div>   </div>
+  </div>
+  <br/>
+  <div class='accordion' id='accordion2'>
+    <div class='accordion-group'>
+      <div class='accordion-heading'>
+        <a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion2' href='#collapseOne'>
+          View More
+        </a>
+      </div>
+      <div id='collapseOne' class='accordion-body collapse'>
+        <div class='accordion-inner'>
+          <div class="field field-name-body field-type-text-with-summary field-label-hidden"><p>Greetings Summoners!<br><br>Here are this week's free champions: <br><br>Week 8 (this week):</p><p><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/corki"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/corki.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/rammus"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/rammus.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/taric"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/taric.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/udyr"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/udyr.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/lux"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/lux.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/caitlyn"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/caitlyn.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/fizz"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/fizz.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/ahri"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/ahri.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/rengar"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/rengar.jpg"></a></span><span class="content-border" style="margin: 10px 5px"><a href="http://gameinfo.na.leagueoflegends.com/en/game-info/champions/khazix"><img src="http://riot-web-static.s3.amazonaws.com/images/news/Champion_Rotation_Cards/English/khazix.jpg"></a></span></p><p>Wondering how we picked this week's free champions? Read up on it <a href="http://na.leagueoflegends.com/board/showthread.php?t=46045"><strong>HERE</strong></a>.</p><p>See you on the Fields of Justice!</p>
+</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</div>
+</body>
+</html>
